270583d4709a9c27d0bb8fb645169efa5ccfaccb
aksharma-7 <atulkumar.sharma711@gmail.com>
2024-02-12 13:28:51 +0530

    Added Create user functionality

---
+++ backend/models/userModel.js
@@ -0,0 +1,29 @@
+import mongoose, { Schema } from "mongoose";
+
+const userSchema = Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      default: false,
+    },
+    isAdmin: {
+      type: Boolean,
+      required: true,
+      default: false,
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
