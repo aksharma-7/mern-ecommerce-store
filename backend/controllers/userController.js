60d4b2b3cdbf075bb80e1706b11e4f9075185424
aksharma-7 <atulkumar.sharma711@gmail.com>
2024-02-12 16:04:17 +0530

    Added logout functionality

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,4 +59,13 @@ const loginUser = asyncHandler(async (req, res) => {
   return;
 });
 
-export { createUser, loginUser };
+const logoutCurrentUser = asyncHandler(async (req, res) => {
+  res.cookie("jwt", "", {
+    httpOnly: true,
+    expires: new Date(0),
+  });
+
+  res.status(200).json({ message: "Logged out successfully" });
+});
+
+export { createUser, loginUser, logoutCurrentUser };
