3718a2b4df4c068c22d7e3fde77be7645f4be55a
aksharma-7 <atulkumar.sharma711@gmail.com>
2024-02-21 14:36:11 +0530

    Added Message Component

--- frontend/src/pages/Admin/UserList.jsx
+++ frontend/src/pages/Admin/UserList.jsx
@@ -1,4 +1,13 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import { FaTrash, FaEdit, FaCheck, FaTimes } from "react-icons/fa";
+import Message from "../../components/Message";
+import Loader from "../../components/Loader";
+import {
+  useDeleteUserMutation,
+  useGetUsersQuery,
+  useUpdateUserMutation,
+} from "../../redux/api/usersApiSlice";
+import { toast } from "react-toastify";
 
 const UserList = () => {
   return <div>UserList</div>;
