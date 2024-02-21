0eff75b192da19e9b50dcc57fbf7e39b6431052a
aksharma-7 <atulkumar.sharma711@gmail.com>
2024-02-21 15:51:17 +0530

    Added create category api

---
+++ backend/routes/categoryRoutes.js
@@ -0,0 +1,9 @@
+import express from "express";
+import { createCategory } from "../controllers/categoryController.js";
+import { authenticate, authorizeAdmin } from "../middlewares/authMiddleware.js";
+
+const router = express.Router();
+
+router.route("/").post(authenticate, authorizeAdmin, createCategory);
+
+export default router;
