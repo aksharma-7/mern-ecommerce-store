bbf54fcad1d7aa8eef4fedb4a73e80998ec8feba
aksharma-7 <atulkumar.sharma711@gmail.com>
2024-02-21 14:29:03 +0530

    Added Profile Component

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -12,10 +12,15 @@ import { Provider } from "react-redux";
 import store from "./redux/store.js";
 import Login from "./pages/Auth/Login.jsx";
 import Register from "./pages/Auth/Register.jsx";
+import PrivateRoute from "./components/PrivateRouter.jsx";
+import Profile from "./pages/User/Profile.jsx";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
+      <Route path="" element={<PrivateRoute />}>
+        <Route path="/profile" element={<Profile />} />
+      </Route>
       <Route path="/login" element={<Login />} />
       <Route path="/register" element={<Register />} />
     </Route>
